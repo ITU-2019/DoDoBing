89098c3a0ac5fccb9a66e95d1f6d7c6f1217a5b8
Anders Munck <Jubas@users.noreply.github.com>
2017-09-25 14:36:39 +0200

    Added parsing, simple documentation and tests for parser. (#5)
    * Added parsing, simple documentation and tests for parser.
    
    * Further testing, fix forgotten int parsing

--- gorilla/src/main.py
+++ gorilla/src/main.py
@@ -1,5 +1,7 @@
 '''Imports'''
-
+import sys
+import re
+import string
 '''Imports end'''
 '''Fields'''
 
@@ -7,11 +9,66 @@
 
 '''Parse'''
 
+
 def parse_dna_file(filename):
-    pass
+    '''
+    Parse file to an array of tuples (name, dna-string)
+    @param: filename: path of file to parse
+    '''
+    dna_array = []
+    dna_counter = -1
+    with open(filename) as f:
+        lines = f.readlines()
+        cur_species = ""
+        cur_dna = ""
+        for line in lines:
+            if line[0] == ">":
+                #save prev species
+                if dna_counter != -1:
+                    dna_array.insert(dna_counter,(cur_species, cur_dna))
+
+                #new species
+                line_cleaned = line[1:]
+                cur_species = line_cleaned.split()[0]
+                cur_dna = ""
+                dna_counter += 1
+                continue
+            line_newline_cleaned = line.rstrip()
+            cur_dna += line_newline_cleaned
+
+        #save last species occurence
+        dna_array.insert(dna_counter,(cur_species, cur_dna))
+    return dna_array
 
 def parse_penalty_file(filename):
-    pass
+    '''
+    Parse file to a dict of dicts {enzyme,{enzyme, score}}
+    @param: filename: path of file to parse
+    '''
+    penalty_dict = {}
+
+    with open(filename) as f:
+        lines = f.readlines()
+        line_count = 0
+        map_array = []
+        for line in lines:
+            if line[0] == "#":
+                continue
+
+            line_space_cleaned = line.split()
+            if line_count == 0:
+                # read to tmparr
+                map_array = line_space_cleaned
+                line_count +=1
+                continue
+
+            # set new dict
+            penalty_dict[line_space_cleaned[0]] = {}
+            for x in range(1, len(line_space_cleaned)):
+                penalty_dict[line_space_cleaned[0]][map_array[(int(x)-1)]] = int(line_space_cleaned[x])
+
+            line_count += 1
+    return penalty_dict
 
 '''Parse end'''
 
@@ -25,7 +82,7 @@ def output(result):
 
 '''Algorithm'''
 def space_efficient_alignment():
-    
+    pass
 
 def backward_space_efficient_alignment():
     pass
@@ -44,22 +101,23 @@ def devide_and_conquer_alignment(dna_1, dna_2):
     # devide_and_conquer_alignment(dna_1[1:q], dna_2[1:n/2])
     # devide_and_conquer_alignment(dna_1[q + 1:n], dna_2[n/2 +1:n])
     # return P....
+    pass
 
-def main_algo(all_dna, all_penalty):
+'''def main_algo(all_dna, all_penalty):
     for i in range(len(all_dna)):
         for j in range (i, len(all_dna)):
             if i != j:
                 res = devide_and_conquer_alignment(all_dna[i], all_dna[j], all_penalty)
-                output(res)
+                output(res)'''
 '''Algorithm'''
 
 '''RUN CODE'''
 if __name__ == "__main__":
     args = sys.argv
-    if len(args) > 1:
+    if len(args) > 2:
         all_dna = parse_dna_file(args[1])
         all_penalty = parse_penalty_file(args[2])
-        main_algo(all_dna, all_penalty)
+        #main_algo(all_dna, all_penalty)
         
 '''END CODE'''
 
--- gorilla/src/test.py
+++ gorilla/src/test.py
@@ -1,3 +1,5 @@
+from main import *
+
 def test_1():
     assert 1 == 1
 
@@ -6,3 +8,18 @@ def test_2():
 
 def test_3():
     assert 2 == 2
+
+def test_parse_toys():
+    dna_parsed = parse_dna_file("../data/Toy_FASTAs-in.txt")
+    assert len(dna_parsed) == 3
+    assert dna_parsed[1][1] == "KAK"
+
+def test_parse_real():
+    dna_parsed = parse_dna_file("../data/HbB_FASTAs-in.txt")
+    assert len(dna_parsed) == 13
+    assert dna_parsed[2][0] == "Gorilla"
+
+def test_parse_penalties():
+    penalties_parsed = parse_penalty_file("../data/BLOSUM62.txt")
+    assert len(penalties_parsed) == 24
+    assert penalties_parsed['W']['W'] == 11
