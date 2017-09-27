fd075e1781a87132e51a7dc43eef417d5b32fd3c
Mathias Grundtvig Andreasen <mgapcdev@gmail.com>
2017-09-27 12:38:59 +0200

    Main algorithm (#3)
    * reawake skeleton
    
    * Add pass
    
    * Add more OS's for travis
    
    * Remove os abort!
    
    * Added test, broke build
    
    * Added more tests
    
    * Update test.py
    
    * Add Compilable main.py and test
    
    * Add Test of Alg
    
    * Add space_efficient_method
    
    * flue knipper
    
    * it works
    
    * Added real test

--- gorilla/src/main.py
+++ gorilla/src/main.py
@@ -75,26 +75,74 @@ def parse_penalty_file(filename):
 
 '''Helper methods'''
 
-def output(result):
-    pass
+def output(dna_1, dna_2, result):
+    print(dna_1[0] + "--" + dna_2[0] + ": " +  str(result[0]))
+    print(dna_1[1])
+    print(result[1])
 
 '''Helper methods end'''
 
 '''Algorithm'''
-def space_efficient_alignment():
-    pass
+def alignment(dna_1, dna_2, all_penalty):
+    a = []
+    for i in range(0, len(dna_1[1])+1):
+        a.append([])
+        for j in range(0, len(dna_2[1])+1):
+            a[i].append((0,""))
+
+
+    for i in range(1, len(dna_1[1])+1):
+        a[i][0] = (-4 * i, "-" + a[i-1][0][1])
+    for j in range(1, len(dna_2[1])+1):
+        a[0][j] = (-4 * j, "-" + a[0][j-1][1])
+    for j in range(1,len(dna_2[1])+1):
+        for i in range(1,len(dna_1[1])+1):
+            left = -4 + a[i][j-1][0]
+            down = -4 + a[i-1][j][0]
+            cross = all_penalty[dna_1[1][i-1]][dna_2[1][j-1]] + a[i-1][j-1][0]
+            if(left > down and left > cross):
+                a[i][j] = (left, a[i][j-1][1] + "-")
+            elif down > left and down > cross:
+                a[i][j] = (down , a[i-1][j][1] + "-")
+            else :
+                a[i][j] = (cross , a[i-1][j-1][1] + dna_2[1][j-1])
+    return a[len(dna_1[1])][len(dna_2[1])]
+
+
+def space_efficient_alignment(dna_1, dna_2, all_penalty):
+    a = []
+    for i in range(0, 2):
+        a.append([])
+        for j in range(0, len(dna_1[1])+1):
+            a[i].append((0,""))
+    for i in range(1, len(dna_1[1])+1):
+        a[0][i] = (-4 * i, "-" + a[0][i-1][1])
+    for j in range(1,len(dna_2[1])+1):
+        a[j%2][0] = (-4 * j, "-" + a[j%2-1][0][1])
+        for i in range(1,len(dna_1[1])+1):
+            left = -4 + a[j%2-1][i][0]
+            down = -4 + a[j%2][i-1][0]
+            cross = all_penalty[dna_1[1][i-1]][dna_2[1][j-1]] + a[j%2-1][i-1][0]
+            if(left > down and left > cross):
+                a[j%2][i] = (left, a[j%2-1][i][1] + "-")
+            elif down > left and down > cross:
+                a[j%2][i] = (down , a[j%2][i-1][1] + "-")
+            else :
+                a[j%2][i] = (cross , a[j%2-1][i-1][1] + dna_2[1][j-1])
+            letter = j
+    return a[len(dna_2[1]) % 2][len(dna_1[1])]
 
 def backward_space_efficient_alignment():
     pass
 
-def devide_and_conquer_alignment(dna_1, dna_2):
-    # let m be the number of ymbols in dna_1
+def devide_and_conquer_alignment(dna_1, dna_2, all_penalty):
+    # let m be the number of symbols in dna_1
     # let n be the number of symbols in dna_2
-    # if m <= 2 or n <= then 
-        # compute optimal alignment using Allignemnt(X,Y)
+    # if m <= 2 or n <= 2 then
+        # compute optimal alignment using Allignment(X,Y)
     # call Space-Efficient-Alignment (X,Y[1:n/2])
     # Call Backward_Space_efficient_alignment(X,Y[n/2 +1:n])
-    
+
     # Let q be the index minimizing f(q, n/2)+ g(q, n/2)
     # add (q,n/2) to global list P
 
@@ -103,12 +151,13 @@ def devide_and_conquer_alignment(dna_1, dna_2):
     # return P....
     pass
 
-'''def main_algo(all_dna, all_penalty):
+def main_algo(all_dna, all_penalty):
     for i in range(len(all_dna)):
         for j in range (i, len(all_dna)):
             if i != j:
-                res = devide_and_conquer_alignment(all_dna[i], all_dna[j], all_penalty)
-                output(res)'''
+                res = space_efficient_alignment(all_dna[j], all_dna[i], all_penalty)
+                output(all_dna[i], all_dna[j], res)
+
 '''Algorithm'''
 
 '''RUN CODE'''
@@ -117,7 +166,6 @@ if __name__ == "__main__":
     if len(args) > 2:
         all_dna = parse_dna_file(args[1])
         all_penalty = parse_penalty_file(args[2])
-        #main_algo(all_dna, all_penalty)
-        
-'''END CODE'''
+        main_algo(all_dna, all_penalty)
 
+'''END CODE'''
--- gorilla/src/test.py
+++ gorilla/src/test.py
@@ -1,5 +1,7 @@
 from main import *
 
+penalties_parsed = parse_penalty_file("../data/BLOSUM62.txt")
+
 def test_parse_toys():
     dna_parsed = parse_dna_file("../data/Toy_FASTAs-in.txt")
     assert len(dna_parsed) == 3
@@ -11,6 +13,106 @@ def test_parse_real():
     assert dna_parsed[2][0] == "Gorilla"
 
 def test_parse_penalties():
-    penalties_parsed = parse_penalty_file("../data/BLOSUM62.txt")
     assert len(penalties_parsed) == 24
     assert penalties_parsed['W']['W'] == 11
+    assert penalties_parsed['A']['A'] == 4
+    assert penalties_parsed['M']['N'] == -2
+    assert penalties_parsed['A']['Q'] == -1
+    assert penalties_parsed['T']['B'] == -1
+    assert penalties_parsed['*']['*'] == 1
+    assert penalties_parsed['A']['*'] == -4
+
+def test_board_1_alg():
+    result = alignment(("First", "AFCD"), ("Second", "BQXF"), penalties_parsed)
+    sresult = result[0]
+    assert sresult == -10
+
+def test_board_2_alg():
+    result = alignment(("First", "AFCD"), ("Second", "BQXF"), penalties_parsed)
+    assert result[1] == "BQXF"
+
+def test_board_3_alg():
+    result = alignment(("First", "AFC"), ("Second", "BQX"), penalties_parsed)
+    assert result[1] == "BQX"
+    assert result[0] == -7
+
+def test_board_4_alg():
+    result = alignment(("First", "AF"), ("Second", "BQ"), penalties_parsed)
+    assert result[1] == "BQ"
+    assert result[0] == -5
+
+def test_board_5_alg():
+    result = alignment(("First", "A"), ("Second", "B"), penalties_parsed)
+    assert result[1] == "B"
+    assert result[0] == -2
+
+def test_board_6_alg():
+    result = alignment(("First", "W"), ("Second", "D"), penalties_parsed)
+    assert result[1] == "D"
+    assert result[0] == -4
+
+def test_board_7_alg():
+    result = alignment(("First", "AFC"), ("Second", "B"), penalties_parsed)
+    assert result[1] == "B--"
+    assert result[0] == -10
+
+def test_board_8_alg():
+    result = alignment(("First", "A"), ("Second", "BQX"), penalties_parsed)
+    assert result[1] == "--X"
+    assert result[0] == -8
+
+def test_space_efficient1():
+    result = space_efficient_alignment(("First", "AFCD"), ("Second", "BQXF"), penalties_parsed)
+    sresult = result[0]
+    assert sresult == -10
+
+def test_space_efficient2():
+    result = space_efficient_alignment(("First", "AFCD"), ("Second", "BQXF"), penalties_parsed)
+    assert result[1] == "BQXF"
+
+def test_space_efficient3():
+    result = space_efficient_alignment(("First", "AFC"), ("Second", "BQX"), penalties_parsed)
+    assert result[1] == "BQX"
+    assert result[0] == -7
+
+def test_space_efficient4():
+    result = space_efficient_alignment(("First", "AF"), ("Second", "BQ"), penalties_parsed)
+    assert result[1] == "BQ"
+    assert result[0] == -5
+
+def test_space_efficient5():
+    result = space_efficient_alignment(("First", "A"), ("Second", "B"), penalties_parsed)
+    assert result[1] == "B"
+    assert result[0] == -2
+
+def test_space_efficient6():
+    result = space_efficient_alignment(("First", "W"), ("Second", "D"), penalties_parsed)
+    assert result[1] == "D"
+    assert result[0] == -4
+
+def test_space_efficient7():
+    result = space_efficient_alignment(("First", "AFC"), ("Second", "B"), penalties_parsed)
+    assert result[1] == "B--"
+    assert result[0] == -10
+
+def test_space_efficient8():
+    result = space_efficient_alignment(("First", "A"), ("Second", "BQX"), penalties_parsed)
+    assert result[1] == "--X"
+    assert result[0] == -8
+
+
+def test_space_efficient_real1():
+    result = space_efficient_alignment(
+        ("Human", "MVHLTPEEKSAVTALWGKVNVDEVGGEALGRLLVVYPWTQRFFESFGDLSTPDAVMGNPKVKAHGKKVLGAFSDGLAHLDNLKGTFATLSELHCDKLHVDPENFRLLGNVLVCVLAHHFGKEFTPPVQAAYQKVVAGVANALAHKYH"),
+        ("Goul", "VHWSAEEKQLITGLWGKVNVADCGAEALARLLIVYPWTQRFFASFGNLSSPTAINGNPMVRAHGKKVLTSFGEAVKNLDNIKNTFAQLSELHCDKLHVDPENFRLLGDILIIVLAAHFAKDFTPDSQAAWQKLVRVVAHALARKYH"),
+        penalties_parsed
+    )
+    assert result[0] == 532
+
+def test_space_efficient_real2():
+    result = space_efficient_alignment(
+        ("Trout", "VEWTDAEKSTISAVWGKVNIDEIGPLALARVLIVYPWTQRYFGSFGNVSTPAAIMGNPKVAAHGKVVCGALDKAVKNMGNILATYKSLSETHANKLFVDPDNFRVLADVLTIVIAAKFGASFTPEIQATWQKFMKVVVAAMGSRYF"),
+        ("Rockcod", "VEWTDKERSIISDIFSHMDYDDIGPKALSRCLIVYPWTQRHFSGFGNLYNAEAIIGNANVAAHGIKVLHGLDRGVKNMDNIAATYADLSTLHSEKLHVDPDNFKLLSDCITIVLAAKMGHAFTAETQGAFQKFLAVVVSALGKQYH"),
+        penalties_parsed
+    )
+    assert result[0] == 442
