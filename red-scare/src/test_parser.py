dedaa9d7eada3e7b7e5ea825c0c61878b657a547
Wrede <sbwr@itu.dk>
2017-11-06 12:00:18 +0100

    Add Arguments Parsing in Main

--- red-scare/src/test_parser.py
+++ red-scare/src/test_parser.py
@@ -1,9 +1,9 @@
 
 from main import *
 
-(nodes_1, s1, t1, c1, e1) = parse_red_file("../data/common-1-20.txt")
-(nodes_2, s2, t2, c2, e2) = parse_red_file("../data/common-1-100.txt")
-(nodes_3, s3, t3, c3, e3) = parse_red_file("../data/increase-n8-1.txt")
+(nodes_1, s1, t1, c1, n1, e1) = parse_red_file("../data/common-1-20.txt")
+(nodes_2, s2, t2, c2, n2, e2) = parse_red_file("../data/common-1-100.txt")
+(nodes_3, s3, t3, c3, n3, e3) = parse_red_file("../data/increase-n8-1.txt")
 
 
 
