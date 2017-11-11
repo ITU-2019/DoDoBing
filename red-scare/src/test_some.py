6c05328955cc9fcb47f428240463a7cc524c9acd
seb <seb@bgaard.dk>
2017-11-11 18:03:37 +0100

    added test and it fails.

---
+++ red-scare/src/test_some.py
@@ -0,0 +1,13 @@
+from main import *
+
+(nodes_1, s1, t1, c1, n1, e1) = parse_red_file("../data/common-1-20.txt")
+def test_common_1_20_some():
+    assert s(nodes_1,s1,t1,c1,e1) == False
+
+(nodes_2, s2, t2, c2, n2, e2) = parse_red_file("../data/common-1-100.txt")
+def test_common_1_100_some():
+    assert s(nodes_2,s2,t2,c2,e2) == False
+
+(nodes_3, s3, t3, c3, n3, e3) = parse_red_file("../data/increase-n8-1.txt")
+def test_increase_n8_1_some():
+    assert s(nodes_2,s2,t2,c2,e2) == True
